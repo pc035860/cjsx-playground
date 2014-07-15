042f090dcd6fb9e7eb3b6490309506e0111cd35d
pc035860 <pc035860@gmail.com>
2014-07-16 01:08:12 +0800

    Setup gulp fro playground:
    - Enter `gulp` to start

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util');
 
-var cjsx = require('gulp-cjsx');
+var cjsx = require('gulp-cjsx'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload');
 
-gulp.task('default', function () {
-  gulp.src('**.cjsx', {base: './'})
+var config = {
+  watch: {
+    app: 'public/{,**/}*.{js,html,css}',
+    cjsx: '{,**/}*.cjsx'
+  },
+  appRoot: 'public'
+};
+
+gulp.task('default', ['server', 'watch']);
+
+gulp.task('server', function () {
+  connect.server({
+    root: config.appRoot
+  });
+});
+
+gulp.task('cjsx', function () {
+  gulp.src(config.watch.cjsx, {base: './'})
     .pipe(cjsx({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('**.cjsx', ['default']);
+  livereload.listen();
+  gulp.watch(config.watch.cjsx, ['cjsx']);
+  gulp.watch(config.watch.app).on('change', livereload.changed);
 });
